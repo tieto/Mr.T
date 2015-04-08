c20d1b66a04b3d3a693e7ae6266dc3b80edd0fd0
Jan.5 <jpcxc@126.com>
2015-04-08 13:55:05 +0800

    Improve authority manage detail page & change ActionBar style
    Changed the ActionBar style to display white fond and arrow

---
+++ app/src/main/java/com/tieto/systemmanagement/authority/AbsFragmentActivity.java
@@ -0,0 +1,37 @@
+package com.tieto.systemmanagement.authority;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.view.MenuItem;
+
+import com.tieto.systemmanagement.R;
+
+/**
+ * @author Jiang Ping
+ */
+public abstract class AbsFragmentActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authority_fragment_base);
+        getSupportFragmentManager().beginTransaction().
+                replace(android.R.id.content, onCreateFragment(savedInstanceState)).commit();
+        if (getActionBar() != null) {
+            getActionBar().setIcon(0);
+            getActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    protected abstract Fragment onCreateFragment(Bundle savedInstanceState);
+}
--- app/src/main/java/com/tieto/systemmanagement/authority/AuthorityActivity.java
+++ app/src/main/java/com/tieto/systemmanagement/authority/AuthorityActivity.java
@@ -3,17 +3,13 @@ package com.tieto.systemmanagement.authority;
 import com.tieto.systemmanagement.R;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 
-public class AuthorityActivity extends FragmentActivity {
+public class AuthorityActivity extends AbsFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_authority);
-        getSupportFragmentManager().beginTransaction().
-                replace(android.R.id.content, AuthorityAppListFragment.newInstance()).commit();
-        //noinspection ConstantConditions
-        getActionBar().setIcon(0);
+    protected Fragment onCreateFragment(Bundle savedInstanceState) {
+        return AuthorityAppListFragment.newInstance();
     }
 }
