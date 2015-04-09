37254030c32a5b039f96e68f9bf81c5d64acc8ca
boworking <bo.b.wang@tieto.com>
2015-04-09 18:31:13 +0800

    Disk: real my DICM and Music/Ringtones data introduced

--- app/src/main/java/com/tieto/systemmanagement/diskmonitor/data/DiskData.java
+++ app/src/main/java/com/tieto/systemmanagement/diskmonitor/data/DiskData.java
@@ -7,7 +7,10 @@ import android.os.StatFs;
 import com.tieto.systemmanagement.R;
 import com.tieto.systemmanagement.TApp;
 import com.tieto.systemmanagement.diskmonitor.entity.DStorageInfo;
+import com.tieto.systemmanagement.diskmonitor.utils.FileUtils;
+import com.tieto.systemmanagement.diskmonitor.utils.Mp3Filter;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,6 +19,8 @@ import java.util.List;
  */
 public class DiskData {
     private static final long KILOBYTE = 1024;
+    private static final long Bytes2Megs = 1048576L;
+
     private static DiskData ourInstance = new DiskData();
     private long internalTotal = 0 ;
     private long externalTotal = 0;
@@ -27,10 +32,6 @@ public class DiskData {
     public long getStorageTotal() {
         return  internalTotal + externalTotal;
 
-    }
-
-    private DiskData() {
-        getStorageUsed();
     }
 
     public long getStorageUsed() {
@@ -66,19 +67,19 @@ public class DiskData {
         DStorageInfo info1= new DStorageInfo();
         info1.setIcon(TApp.getInstance().getDrawable(R.drawable.sysclear_file_apk));
         info1.setTitle(TApp.getInstance().getString(R.string.disk_space_store_title_1));
-        info1.setTotal("123M");
+        info1.setTotal("0M");
         spaceInfos.add(info1);
 
         DStorageInfo info2= new DStorageInfo();
         info2.setIcon(TApp.getInstance().getDrawable(R.drawable.sysclear_file_audio));
         info2.setTitle(TApp.getInstance().getString(R.string.disk_space_store_title_2));
-        info2.setTotal("123M");
+        info2.setTotal(displaySize(getAlbumSize()));
         spaceInfos.add(info2);
 
         DStorageInfo info3= new DStorageInfo();
         info3.setIcon(TApp.getInstance().getDrawable(R.drawable.sysclear_file_zip));
         info3.setTitle(TApp.getInstance().getString(R.string.disk_space_store_title_3));
-        info3.setTotal("123M");
+        info3.setTotal(displaySize(getAudioSize()));
         spaceInfos.add(info3);
 
         return spaceInfos;
@@ -96,6 +97,65 @@ public class DiskData {
         return systemInfos;
     }
 
+    //get the song list from sd-card
+    public List<String> getAudioList() {
+        List<String> audioList = new ArrayList<String>();
+        File home = new File("/");
+
+        if (home.listFiles(new Mp3Filter()).length > 0) {
+            for (File file : home.listFiles(new Mp3Filter())) {
+                audioList.add(file.getName());
+            }
+        }
+        return audioList;
+    }
+
+    //TODO: private functions
+    private DiskData() {
+        getStorageUsed();
+    }
+
+    private long getAudioSize() {
+        long total = 0;
+        String path;
+
+        if (FileUtils.isExternalStorageReadable()){
+            path = Environment.getExternalStoragePublicDirectory(
+                    Environment.DIRECTORY_RINGTONES).getAbsolutePath();
+            total += FileUtils.getSize(new File(path));
+
+            path = Environment.getExternalStoragePublicDirectory(
+                    Environment.DIRECTORY_MUSIC).getAbsolutePath();
+            total +=  FileUtils.getSize(new File(path));
+        }
+
+        return total;
+    }
+
+    private String displaySize(long size) {
+        if (size > Bytes2Megs) {
+            size/= Bytes2Megs;
+            return  size+"M";
+        }
+        else if (size > KILOBYTE) {
+            size/=KILOBYTE;
+            return size+"K";
+        }
+        return size+"B";
+    }
+
+   private long getAlbumSize() {
+        long total = 0;
+
+        if (FileUtils.isExternalStorageReadable())
+        {
+            String path = Environment.getExternalStoragePublicDirectory(
+                    Environment.DIRECTORY_DCIM).getAbsolutePath();
+            total = FileUtils.getSize(new File(path));
+        }
+
+        return total;
+    }
 
 //    public ArrayList<DProcessInfo> getPackages() {
 //        ArrayList<DProcessInfo> apps = getInstalledApps(false); /* false = no system packages */
---
+++ app/src/main/java/com/tieto/systemmanagement/diskmonitor/utils/FileUtils.java
@@ -0,0 +1,45 @@
+package com.tieto.systemmanagement.diskmonitor.utils;
+
+import android.os.Environment;
+
+import java.io.File;
+
+/**
+ * Created by wangbo on 4/9/15.
+ */
+public class FileUtils {
+    /* Checks if external storage is available for read and write */
+    public static boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+
+    /* Checks if external storage is available to at least read */
+    public static boolean isExternalStorageReadable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state) ||
+                Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+
+
+    public static long getSize(File f) {
+        if(!(f.exists())) return 0;
+
+       long size = 0;
+        if (f.isDirectory()) {
+
+            for (File file : f.listFiles()) {
+                size += getSize(file);
+            }
+        } else {
+            size=f.length();
+        }
+        return size;
+    }
+}
